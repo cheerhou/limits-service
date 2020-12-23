7d784f8fdcc32208c1b600916cece93029c95044
Chenjin Hou <houchenjin@23mofang.com>
2020-12-23 15:33:00 +0800

    using values in the properties file

---
+++ src/main/java/com/cheerhou/microservices/limitsservice/Configuration.java
@@ -0,0 +1,32 @@
+package com.cheerhou.microservices.limitsservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hcj
+ * @Description
+ * @Date 2020/12/23
+ */
+@Component
+@ConfigurationProperties("limits-services")
+public class Configuration {
+    private int minimum;
+    private int maximum;
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+}
--- src/main/java/com/cheerhou/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/cheerhou/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.cheerhou.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class LimitsConfigurationController {
+    @Autowired
+    private Configuration configuration;
+
 
     @GetMapping("/limits")
     public  LimitsConfiguration retrieveLimitsFromConfiguration() {
-        return new LimitsConfiguration(1000, 1);
+        return new LimitsConfiguration(configuration.getMaximum(), configuration.getMinimum());
     }
 }
